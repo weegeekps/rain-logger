c2f58787a25299978c3beb6527b480f19ab5169f
Adam Morris <adam@kernelpanicstudios.com>
2021-04-08 01:26:53 -0500

    Stubbed out the auth controller.

--- server/src/routes/auth.rs
+++ server/src/routes/auth.rs
@@ -1,1 +1,27 @@
-pub fn insert
+use rocket_contrib::json::Json;
+use diesel::PgConnection;
+use crate::DbConn;
+use std::error::Error;
+use uuid::Uuid;
+use chrono::prelude::*;
+
+#[derive(Deserialize)]
+struct LoginRequest {
+    name: String,
+    password: String,
+}
+
+#[derive(Serialize)]
+struct LoginResponse {
+    token: Uuid,
+    valid_until: DateTime<Utc>
+}
+
+#[post("/auth/login", data = <payload>)]
+pub fn login(conn: DbConn, payload: Json<LoginRequest>) -> Result<Json<LoginResponse>, Box<dyn Error>> {
+    todo!("Implement login")
+}
+
+pub fn logout() {
+    todo!("Implement logout")
+}
