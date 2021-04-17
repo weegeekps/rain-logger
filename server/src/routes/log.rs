9da07682921e02039145cae28149a35f0318674c
Adam Morris <adam@kernelpanicstudios.com>
2021-04-17 13:15:22 -0500

    Implemented create and delete log entry endpoints.

--- server/src/routes/log.rs
+++ server/src/routes/log.rs
@@ -1,12 +1,34 @@
+use chrono::{DateTime, Utc};
 use diesel::PgConnection;
 use log::{debug, error};
 use rocket::http::Status;
 use rocket_contrib::json::Json;
-use uuid::{Error, Uuid};
+use uuid::Uuid;
 
 use crate::DbConn;
 use crate::models::auth::Auth;
-use crate::models::precipitation_log::PrecipitationLog;
+use crate::models::precipitation_log::{PrecipitationLog, PrecipitationType};
+
+#[derive(Deserialize, Clone)]
+pub struct CreateEntryRequest {
+    pub measurement: f32,
+    pub logged_at: DateTime<Utc>,
+    pub notes: Option<String>,
+    pub ptype: i16,
+    pub anomaly: bool,
+}
+
+impl CreateEntryRequest {
+    pub fn into_precipitation_log(self) -> PrecipitationLog {
+        PrecipitationLog::new(
+            self.measurement,
+            self.logged_at,
+            PrecipitationType::from_i16(self.ptype),
+            self.notes,
+            self.anomaly,
+        )
+    }
+}
 
 #[get("/logs/entries")]
 pub fn get_all_entries(conn: DbConn, _auth: &Auth) -> Result<Json<Vec<PrecipitationLog>>, Status> {
@@ -42,8 +64,15 @@ pub fn get_entry(conn: DbConn, _auth: &Auth, id: String) -> Result<Json<Precipit
 }
 
 #[post("/logs/entry", data = "<entry>")]
-pub fn create_entry(conn: DbConn, _auth: &Auth, entry: Json<PrecipitationLog>) -> Result<Json<PrecipitationLog>, Status> {
-    todo!("implement create_entry")
+pub fn create_entry(conn: DbConn, _auth: &Auth, entry: Json<CreateEntryRequest>) -> Result<Json<PrecipitationLog>, Status> {
+    let new_entry = entry.clone().into_precipitation_log();
+    match PrecipitationLog::create(&conn as &PgConnection, &new_entry) {
+        Ok(result) => Ok(Json(result)),
+        Err(err) => {
+            error!("{}", err.to_string());
+            Err(Status::InternalServerError)
+        }
+    }
 }
 
 #[put("/logs/entry/<id>", data = "<entry>")]
@@ -71,5 +100,19 @@ pub fn update_entry(conn: DbConn, _auth: &Auth, id: String, entry: Json<Precipit
 
 #[delete("/logs/entry/<id>")]
 pub fn delete_entry(conn: DbConn, _auth: &Auth, id: String) -> Result<Status, Status> {
-    todo!("implement delete_entry")
+    let parsed_id = match Uuid::parse_str(id.as_str()) {
+        Ok(id) => id,
+        Err(err) => {
+            debug!("{}", err.to_string());
+            return Err(Status::BadRequest);
+        }
+    };
+
+    match PrecipitationLog::delete(&conn as &PgConnection, parsed_id) {
+        Ok(_) => Ok(Status::Ok),
+        Err(err) => {
+            error!("{}", err.to_string());
+            Err(Status::InternalServerError)
+        }
+    }
 }
